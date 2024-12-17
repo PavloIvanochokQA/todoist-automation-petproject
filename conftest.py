2be4596abfe8c2783329f3fc5dbf86224d93f120
Pavlo-Ivanochok <ivanochokpavlo2@gmail.com>
2024-12-17 16:58:12 +0200

    conftest updated

--- conftest.py
+++ conftest.py
@@ -20,6 +20,7 @@ def driver(request):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-blink-features=AutomationControlled")
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
     yield driver
